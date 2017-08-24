f2dce1d7bead88ecabe6a9a08906b71006f67600
June <jeff.bluesky13@gmail.com>
2017-08-24 12:57:09 +0800

    Remove 'Grid' import

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Segment, Image, Grid } from 'semantic-ui-react'
+import { Segment, Image } from 'semantic-ui-react'
 import logo from '../logo.svg'
 
 export default class Footer extends React.Component {
